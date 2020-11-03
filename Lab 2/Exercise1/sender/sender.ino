e43154454e28ae0c6b9aedddce9fd527593d164f
Arne Wouters <arne@wouters.co>
2020-11-03 23:16:48 +0100

    Update sender.ino

--- Lab 2/Exercise1/sender/sender.ino
+++ Lab 2/Exercise1/sender/sender.ino
@@ -41,32 +41,35 @@ void loop() {
   delay(random(1,5)*1000);
 }
 
-int8_t getTemperatureInternal() {
-  // found at https://www.avrfreaks.net/comment/2580401#comment-2580401
-  /* Temperature  °C -45°C  +25°C  +85°C
-     Voltage      mV 242 mV 314 mV 380 mV  */
+double getTemperatureInternal() {
+  // For more details check Section 24 in the datasheet
+  // Sources: 
+  //   - https://syntheticphysical.wordpress.com/2014/01/23/atmega32u2-and-lm36dz-temperature-responses/
+  //   - https://www.avrfreaks.net/comment/2580401#comment-2580401
 
-  // Select Temp Channel and 2.56V Reference
+  // Set the internal reference and mux for the ATmega32U4.
   ADMUX = (1<<REFS1) | (1<<REFS0) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0);
-  ADCSRB |= (1<<MUX5);
-  delay(2); //wait for internal reference to settle
+  ADCSRB |= (1 << MUX5); // enable the ADC
 
-  // start the conversion
-  ADCSRA |= bit(ADSC);
+  delay(2); // Wait for internal reference to settle
 
-  // ADSC is cleared when the conversion finishes
-  while (ADCSRA & bit(ADSC));
+  // Start the conversion
+  ADCSRA |= bit(ADSC); // First reading
 
-  uint8_t low  = ADCL;
-  uint8_t high = ADCH;
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
 
-  //discard first reading
-  ADCSRA |= bit(ADSC);
-  while (ADCSRA & bit(ADSC));
-  low  = ADCL;
-  high = ADCH;
-  int a = (high << 8) | low;
+  ADCSRA |= bit(ADSC); // Second reading
 
-  //return temperature in C
-  return a - 272;
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  byte low = ADCL;
+  byte high = ADCH;
+
+  double t = (high << 8) | low;
+  int offset = -5; // Temperature accuracy is +- 10
+  t = (t - 273 + offset); // Convert from Kelvin to Celcius plus Offset
+
+  return t;
 }
